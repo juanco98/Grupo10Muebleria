82db07dd68a0b0a2d1dc0c0909c3de53863c6178
Juan Cruz Lima <juan.c.lima@live.com>
2022-06-02 21:14:14 -0300

    delete database

--- src/routes/aboutus.js
+++ src/routes/aboutus.js
@@ -1,6 +1,6 @@
 const express   = require('express');
 const router    = express.Router();
-const pool      = require('../database');
+
 
 router.get('/', (req, res) => {
     res.render('aboutus/aboutus');
