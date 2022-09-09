3b22da88ef44a41a2fefcc67e967cff9a4fdf733
Juan Cruz Lima <juan.c.lima@live.com>
2022-09-08 21:36:09 -0300

    FIX CONTACTO

--- src/public/js/user/userFormValidation.js
+++ src/public/js/user/userFormValidation.js
@@ -1,7 +1,39 @@
 window.addEventListener('load', function() {
 
-    let form = document.forms['formUserReg']
-    form.addEventListener("submit", function(e) {
+    let formLog = document.forms['formUserLog']
+    formLog.addEventListener("submit", function(e) {
+
+        let errors = [];
+
+        var emailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
+
+        let email = document.getElementById("#email")
+        let pass = document.getElementById("#pass")
+        if (email.value === '') {
+            errors.push('Email vacio');
+        }
+        if (email.value.match(emailPattern)) {
+            errors.push('Email invalido');
+        }
+        if (pass.value === '') {
+            errors.push('Contraseña vacia');
+        }
+        if (pass.length <= 8) {
+            errors.push('Faltan caracteres');
+        }
+
+        if (errors.length > 0) {
+            e.preventDefault();
+            let ulErr = document.querySelector('.error ul');
+            errors.forEach(error => {
+                ulErr.innerHTML += `<li>${error}</li>`
+            })
+        }
+
+    })
+
+    let formReg = document.forms['formUserReg']
+    formReg.addEventListener("submit", function(e) {
 
         let errors = [];
 
